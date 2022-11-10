b7ca881c164505d9826bf1660b733f1a45900a40
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-10 12:57:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let flipped=0;
+
+let card=document.querySelectorAll('.flip-card-inner');
+console.log('card');
+for (let index = 0; index < card.length; index++) {
+    card[index].addEventListener('click',Flip);    
+}
+
+function Flip(){
+    this.classList.add('flip')
+    flipped+=1;
+    if(flipped==2){
+        flipped=0;
+        ReFlip();
+        for (let index = 0; index < card.length; index++) {
+            card[index].removeEventListener('click',Flip);    
+        }
+    }
+}
+function Control(){
+    let remove=document.querySelectorAll('.flip');
+    for (let index = 0; index < remove.length; index++) {
+        remove[index].classList.remove('flip');  
+    }
+    for (let index = 0; index < card.length; index++) {
+        card[index].addEventListener('click',Flip);    
+    }
+}
+function ReFlip(){
+    setTimeout(Control,1500);
+}
